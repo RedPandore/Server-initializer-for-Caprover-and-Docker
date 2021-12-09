dc3791c777149b0cd463af1c45a1e9d847beec7b
tennessee <houry9@gmail.com>
2021-12-09 10:58:14 +0100

    add link to caprover documentation

--- init/initPc.sh
+++ init/initPc.sh
@@ -11,15 +11,16 @@ while true; do
                 echo -e "${GREEN}Continue${NC}"
                 echo -e "${BLUE}Configuring your local machine...${NC}"
                 echo -e "${BLUE}Updating ...${NC}"
-                sudo apt update -y
-                sudo apt upgrade -y
+                sudo apt update -y &>/dev/null
+                sudo apt upgrade -y &>/dev/null
                 echo -e "${GREEN}DONE${NC}"
                 echo -e "${BLUE}Installing Caprover...${NC}"
-                npm install -g caprover -y 
+                npm install -g caprover -y &>/dev/null
                 echo -e "${GREEN}DONE${NC}"
                 echo -e "${BLUE}Launching caprover Setup ${NC}"
                 echo -e "${RED}Note: It will not be possible to carry through with the 'caprover serversetup' if you've already forced https on your CapRover instance.${NC}"
                 echo -e "${RED}Note: In such case go straight to logging in with the caprover login command. To change the password go to the settings menu in the app.${NC}"
+                echo -e "${BLUE}You can get help for this part here => https://caprover.com/docs/get-started.html#step-2-connect-root-domain${NC}"
                 caprover serversetup
                 echo -e "${GREEN}DONE${NC}"
 
